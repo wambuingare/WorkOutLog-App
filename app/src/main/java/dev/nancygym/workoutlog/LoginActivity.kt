7001c9bd9a6b60e8702bf43eb95f86b7976e4016
Wambuingare <wambuinancyngare@gmail.com>
2022-06-02 21:14:35 +0300

    Added LoginActivity

---
+++ app/src/main/java/dev/nancygym/workoutlog/LoginActivity.kt
@@ -0,0 +1,46 @@
+package dev.nancygym.workoutlog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var btnLogin: Button
+    lateinit var tilEmail: TextInputLayout
+    lateinit var etEmail: TextInputEditText
+    lateinit var tilpassword: TextInputLayout
+    lateinit var etpassword: TextInputEditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        btnLogin = findViewById(R.id.btnLogin)
+        tilEmail = findViewById(R.id.tilEmail)
+        etEmail = findViewById(R.id.etEmail)
+        tilpassword = findViewById(R.id.tilPassword)
+        etpassword = findViewById(R.id.etPassword)
+        btnLogin.setOnClickListener {validatelogin()}
+
+    }
+        fun validatelogin() {
+            var error = false
+            tilEmail.error = null
+            tilpassword.error = null
+            var email = etEmail.text.toString()
+            if (email.isBlank()) {
+                tilEmail.error = "Email is required"
+                error = true
+            }
+            var password = etpassword.text.toString()
+            if (password.isBlank()) {
+                tilpassword.error = "Password is required"
+                error = true
+            }
+            if (error != true) {
+            }
+        }
+    }
+
